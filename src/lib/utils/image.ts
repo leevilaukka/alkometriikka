d28fcc46c9ad57fbfe8ff6ecd1cf81083bf5dbe9
Leevi Laukka <leevi.laukka@gmail.com>
2025-09-20 03:10:43 +0300

    fix: optimize images

--- src/lib/utils/image.ts
+++ src/lib/utils/image.ts
@@ -1,11 +1,11 @@
-const IMAGE_BASE_URL: string = 'https://images.alko.fi/images/cs_srgb,f_auto,t_medium/cdn'
 
-export const generateImageUrl = (itemNumber: string, itemName: string): string => {
+export const generateImageUrl = (itemNumber: string, itemName: string, transform?: "products" | "medium"): string => {
+    const imageURL =`https://images.alko.fi/images/cs_srgb,f_auto,t_${transform || "products"}/cdn`
     if(!itemName.toLowerCase) console.warn("itemName has no toLowerCase method:", itemName)
     itemName = String(itemName)
     const sanitizedItemName = itemName
         .toLowerCase()
         .replace(/[^a-z0-9]+/g, '-') // Replace non-alphanumeric characters with hyphens
         .replace(/^-+|-+$/g, '');    // Remove leading and trailing hyphens
-    return `${IMAGE_BASE_URL}/${itemNumber}/${sanitizedItemName}.jpg`;
+    return `${imageURL}/${itemNumber}/${sanitizedItemName}.jpg`;
 }
