a4c53c037538b5c0eb592bf24aa87ab0c748c2b5
Leevi Laukka <leevi.laukka@gmail.com>
2026-08-16 00:54:11 +0300

    fix: Alko changed the product page path, redirect from `/[lang]/tuotteet/[id]` to `/tuotteet/[id]`

---
+++ src/routes/[lang]/tuotteet/[...id]/+page.ts
@@ -0,0 +1,11 @@
+import { redirect } from "@sveltejs/kit";
+
+export async function load({ parent, params }) {
+    const id = params.id?.split('/')[0];
+    if (!id) {
+        throw redirect(300, "/");
+    } else {
+        throw redirect(300, `/tuotteet/${id}`);
+    }  
+}
+ 
